e502ec68c8454ccd341007403bc13d4ec65148d6
dev-ajijul-islam <devajijulislam@gmail.com>
2026-02-05 16:00:48 +0600

    firebase setup created

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,23 @@
+import 'dart:ui';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:quick_buy/app/quick_buy.dart';
+import 'package:quick_buy/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+
   runApp(QuickBuy());
 }
